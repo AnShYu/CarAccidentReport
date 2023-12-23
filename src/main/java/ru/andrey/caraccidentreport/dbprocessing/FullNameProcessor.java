95d95f463ac761d378efb01237d357d159e559e4
AnShYu <andrey.shirokinskiy@gmail.com>
2023-12-23 16:03:16 +0300

    Fixed code. doGet works via sql request

--- src/main/java/ru/andrey/caraccidentreport/dbprocessing/FullNameProcessor.java
+++ src/main/java/ru/andrey/caraccidentreport/dbprocessing/FullNameProcessor.java
@@ -8,11 +8,6 @@ import java.sql.*;
 public class FullNameProcessor {
 
     public LimitedAccidentData getAccidentData(String surname, String name, String fathersName) {
-        LimitedAccidentData lad = new LimitedAccidentData();
-
-        String accidentAddress = "TestAddress";
-        String accidentDate = "TestDate";
-        String accidentGuilt = "TestGuilt";
 
         Connection connection = null;
         PreparedStatement pstmt = null;
@@ -23,10 +18,11 @@ public class FullNameProcessor {
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/andrey",
                     "andrey", "andrey");
 
-            String getDriverID = "select a.province, a.city, a.district, a.street, a.bld_number, a.driver_claimed_guilt, a.accident_time from accident a\n" +
-                    "\twhere id = (select ap.accident_id from accident_participant ap \n" +
-                    "\t\twhere ap.driver_id = (select p.id from persons p\n" +
-                    "\t\t\twhere p.surname=? and p.first_name = ? and p.fathers_name = ?));";
+            String getDriverID = "select a.province, a.city, a.district, a.street, a.bld_number, a.driver_claimed_guilt, a.accident_time from car_accident_report.accident a " +
+                    "where id = (select ap.accident_id from car_accident_report.accident_participant ap " +
+                    "where ap.driver_id = (select p.id from car_accident_report.persons p " +
+                    "where p.surname=? and p.first_name = ? and p.fathers_name = ?));";
+
             pstmt = connection.prepareStatement(getDriverID);
 
             pstmt.setString(1, surname);
@@ -34,17 +30,28 @@ public class FullNameProcessor {
             pstmt.setString(3, fathersName);
 
             ResultSet rs = pstmt.executeQuery();
+            String province = null;
+            String city = null;
+            String district = null;
+            String street = null;
+            String building = null;
+            Integer guilt = null;
+            Timestamp time = null;
 
             if(rs.next()) {
-                int personId = rs.getInt("id");
+                province = rs.getString("province");
+                city = rs.getString("city");
+                district = rs.getString("district");
+                street = rs.getString("street");
+                building = rs.getString("bld_number");
+                guilt = rs.getInt("driver_claimed_guilt");
+                time = rs.getTimestamp("accident_time");
             }
-            //TODO
-            String join = "select id from persons ";
 
+            String address = makeAddress(province, city, district, street, building);
+            LimitedAccidentData lad = makeLad(address, guilt, time);
 
-
-
-
+            return lad;
 
         } catch (ClassNotFoundException e) {
             throw new DataAccessException("Postgres Driver Error", e);
@@ -58,12 +65,50 @@ public class FullNameProcessor {
                 throw new RuntimeException("RunTimeException", e);
             }
         }
+    }
 
-        lad.setAddress(accidentAddress);
-        lad.setDate(accidentDate);
-        lad.setGuilt(accidentGuilt);
-
+    private LimitedAccidentData makeLad (String address, int guilt, Timestamp time) {
+        LimitedAccidentData lad = new LimitedAccidentData();
+        lad.setAddress(address);
+        if (guilt != 1) {
+            lad.setGuilt("Вину не признал");
+        }  else {
+            lad.setGuilt("Признал вину");
+        }
+        if (!time.equals(null)) {
+            lad.setDate(time.toString());
+        }
         return lad;
+    }
+
+    private String makeAddress (String province, String city, String district, String street, String building) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (province != null) {
+            stringBuilder.append(province);
+            stringBuilder.append(", ");
+        }
+        if (city != null) {
+            stringBuilder.append(city);
+            stringBuilder.append(", ");
+        }
+        if (district != null) {
+            stringBuilder.append(district);
+            stringBuilder.append(", ");
+        }
+        if (street != null) {
+            stringBuilder.append(street);
+            stringBuilder.append(", ");
+        }
+        if (building != null) {
+            stringBuilder.append(building);
+            stringBuilder.append(", ");
+        }
+        if (stringBuilder.length() != 0 ) {
+            return stringBuilder.toString();
+        } else {
+            return null;
+        }
+
     }
 
 }
