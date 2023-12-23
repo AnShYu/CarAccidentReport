92c343cd01d8a7b24503f7dbdb740fb966e4c0bc
AnShYu <andrey.shirokinskiy@gmail.com>
2023-12-23 11:39:48 +0300

    Changed to single sql request to DB

--- src/main/java/ru/andrey/caraccidentreport/dbprocessing/FullNameProcessor.java
+++ src/main/java/ru/andrey/caraccidentreport/dbprocessing/FullNameProcessor.java
@@ -23,7 +23,10 @@ public class FullNameProcessor {
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/andrey",
                     "andrey", "andrey");
 
-            String getDriverID = "select id from persons where surname=? and first_name=? and fathers_name=?";
+            String getDriverID = "select a.province, a.city, a.district, a.street, a.bld_number, a.driver_claimed_guilt, a.accident_time from accident a\n" +
+                    "\twhere id = (select ap.accident_id from accident_participant ap \n" +
+                    "\t\twhere ap.driver_id = (select p.id from persons p\n" +
+                    "\t\t\twhere p.surname=? and p.first_name = ? and p.fathers_name = ?));";
             pstmt = connection.prepareStatement(getDriverID);
 
             pstmt.setString(1, surname);
